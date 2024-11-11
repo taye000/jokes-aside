0369729e5d3f0fa88bd86b1581cdc08f1ff9b1bc
taye000 <taylorgitari@gmail.com>
2024-11-11 13:22:37 +0300

    favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pluginOptions: {
+    htmlWebpackPlugin: {
+      options: {
+        title: "Jokes Aside", // Set the title here
+      },
+    },
+  },
+});
